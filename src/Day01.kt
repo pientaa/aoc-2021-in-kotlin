97af3a4c309bfd4d907c2486df49bb238d906638
Łukasz Pięta <pieta.lukasz98@gmail.com>
2021-12-01 17:25:19 +0100

    Day 01 completed (not refactored)

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,15 +1,30 @@
 fun main() {
     fun part1(input: List<String>): Int {
-        return input.size
+        val firstMeasurement = input.first().toInt()
+        return input.fold(Pair(firstMeasurement, 0)) { acc: Pair<Int, Int>, new: String ->
+            new.toInt()
+                .let {
+                    if (it > acc.first) Pair(it, acc.second + 1)
+                    else Pair(it, acc.second)
+                }
+        }.second
     }
 
     fun part2(input: List<String>): Int {
-        return input.size
+        return input.windowed(size = 3, step = 1)
+            .map {
+                it.fold(0) { acc: Int, new: String ->
+                    acc + new.toInt()
+                }
+            }
+            .foldIndexed(Pair(0, 0)) { index: Int, acc: Pair<Int, Int>, new: Int ->
+                when {
+                    index == 0 -> Pair(new, 0)
+                    new > acc.first -> Pair(new, acc.second + 1)
+                    else -> Pair(new, acc.second)
+                }
+            }.second
     }
-
-    // test if implementation meets criteria from the description, like:
-    val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
 
     val input = readInput("Day01")
     println(part1(input))
